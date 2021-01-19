b2ec5b852466f089b03917da1787082068a58038
woodelin-florveus <woodelinflorveus@gmail.com>
2021-01-19 15:37:24 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,4 +12,14 @@ class PostsController < ApplicationController
   end
 
   # add create method here
+
+  def create
+    @post = Post.new
+    @post.title = params[:title]
+    @post.description = params[:description]
+    @post.save
+     redirect_to post_path(@post)
+    # redirect_to "/posts/#{@post.id}"
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  resources :posts
+  # resources :posts
+
+  
+  
+  get "/posts/new", to: "posts#new", as: "new_post"
+  post "/posts", to: "posts#create"
+  get "/posts/:id", to: "posts#show", as: 'post'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
